6f4da29f8ab8b013ec2072fb638962ca437125f6
Dan Sorak <dan.sorak@gmail.com>
2018-01-09 22:31:28 -0600

    Added lombok for getter/setter generation Updated dependencies Cleaned up annotations

--- src/main/java/com/experoinc/dropwizard/dsegraph/DseGraphFactory.java
+++ src/main/java/com/experoinc/dropwizard/dsegraph/DseGraphFactory.java
@@ -21,106 +21,76 @@ import com.datastax.driver.dse.DseCluster;
 import com.datastax.driver.dse.DseSession;
 import com.datastax.driver.dse.graph.GraphOptions;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import io.dropwizard.setup.Environment;
 import io.dropwizard.util.Duration;
 import io.dropwizard.validation.MinDuration;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
 import org.hibernate.validator.constraints.NotEmpty;
 
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import java.util.concurrent.TimeUnit;
 
-/**
- * @author Ted Wilmes
- */
 public class DseGraphFactory {
 
     private static final int DEFAULT_PORT = 8182;
 
+    @Getter
+    @Setter
+    @NonNull
     @NotEmpty
     private String[] contactPoints;
 
+    @Getter
+    @Setter
     @Min(1)
     private int port = DEFAULT_PORT;
 
+    @Getter
+    @Setter
+    @NonNull
     @NotEmpty
-    //private String validationQuery = "select release_version from system.local";
     private String validationQuery = "g.V().hasNext()";
+    //private String validationQuery = "select release_version from system.local";
 
-    @NotNull
+    @Getter
+    @Setter
+    @NonNull
     @MinDuration(value = 1, unit = TimeUnit.MILLISECONDS)
     private Duration validationQueryTimeout = Duration.seconds(10);
 
+    @Getter
+    @Setter
+    @NonNull
     @NotEmpty
     private String graphName;
 
-    @JsonProperty
-    public String getGraphName() { return graphName; }
+    @Getter
+    @Setter
+    private String userName;
 
-    @JsonProperty
-    public void setGraphName(String graphName) { this.graphName = graphName; }
+    @Getter
+    @Setter
+    private String password;
 
-    @JsonProperty
-    public String[] getContactPoints() {
-        return contactPoints;
-    }
-
-    @JsonProperty
-    public void setContactPoints(String[] contactPoints) {
-        this.contactPoints = contactPoints;
-    }
-
-    @JsonProperty
-    public int getPort() {
-        return port;
-    }
-
-    @JsonProperty
-    public void setPort(int port) {
-        this.port = port;
-    }
-
-    @JsonProperty
-    public String getValidationQuery() {
-        return validationQuery;
-    }
-
-    @JsonProperty
-    public void setValidationQuery(String validationQuery) {
-        this.validationQuery = validationQuery;
-    }
-
-    @JsonProperty
-    public Duration getValidationQueryTimeout() {
-        return validationQueryTimeout;
-    }
-
-    @NotNull
+    @Getter
+    @Setter
+    @NonNull
     private Duration shutdownTimeout = Duration.seconds(60);
-
-    @JsonProperty
-    public void setValidationQueryTimeout(Duration validationQueryTimeout) {
-        this.validationQueryTimeout = validationQueryTimeout;
-    }
-
-    @JsonProperty
-    public Duration getShutdownTimeout() {
-        return shutdownTimeout;
-    }
-
-    @JsonProperty
-    public void setShutdownTimeout(Duration shutdownTimeout) {
-        this.shutdownTimeout = shutdownTimeout;
-    }
 
     @JsonIgnore
     public DseCluster build(Environment environment) {
-        DseCluster cluster = DseCluster.builder().
-                addContactPoints(contactPoints).
-                withGraphOptions(new GraphOptions().setGraphName(graphName)).
-                build();
+        DseCluster.Builder builder = DseCluster.builder()
+                .addContactPoints(contactPoints)
+                .withGraphOptions(new GraphOptions().setGraphName(graphName));
 
+        if (null != userName && userName.length() > 0 && null != password && password.length() > 0) {
+            builder = builder.withCredentials(userName, password);
+        }
+
+        DseCluster cluster = builder.build();
         DseSession session = cluster.newSession();
 
         environment.lifecycle().manage(new DseGraphManaged(cluster, getShutdownTimeout()));
